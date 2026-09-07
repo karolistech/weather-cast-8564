74d288e70a07ac164e0920b31483bcb8cd4090fd
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-07 15:00:59 +0300

    feat: add weather current ranges

--- src/components/Weather/WeatherCurrent/WeatherCurrent.tsx
+++ src/components/Weather/WeatherCurrent/WeatherCurrent.tsx
@@ -1,9 +1,16 @@
 import { type WeatherCurrent, getWeatherCondition, getWeatherIcon } from "@/api/weather";
 
 import "./WeatherCurrent.css";
+import uiIcons from "@/assets/icons/ui-icons/ui-icons.svg";
 
 type WeatherCurrentProps = {
   current: WeatherCurrent;
+};
+
+type RangeProps = {
+  icon: string;
+  label: string;
+  value: string;
 };
 
 export default function WeatherCurrent({ current }: WeatherCurrentProps) {
@@ -24,6 +31,32 @@ export default function WeatherCurrent({ current }: WeatherCurrentProps) {
             {condition.description}
           </p>
         </div>
+      </div>
+
+      <div className="weather-current__ranges">
+        <Range icon="apparent-temp" label="Real Feel" value={`${current.apparentTemp}°`} />
+        <Range icon="upwards-arrow" label="Max" value={`${current.maxTemp}°`} />
+        <Range icon="downwards-arrow" label="Min" value={`${current.minTemp}°`} />
+      </div>
+    </div>
+  );
+}
+
+function Range({ icon, label, value }: RangeProps) {
+  return (
+    <div className="weather-current__range">
+      <svg className="weather-current__range-icon">
+        <use href={`${uiIcons}#${icon}`} />
+      </svg>
+
+      <div className="weather-current__range-data">
+        <span className="weather-current__range-label">
+          {label}
+        </span>
+
+        <span className="weather-current__range-value">
+          {value}
+        </span>
       </div>
     </div>
   );
